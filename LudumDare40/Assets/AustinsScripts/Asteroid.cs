1aad4a7b133a5fae41f5cc0775019377d5577244
ArcticFire421 <ap682814@ohio.edu>
2017-12-02 17:15:02 -0500

    Test push to master
    Saving

--- LudumDare40/Assets/AustinsScripts/Asteroid.cs
+++ LudumDare40/Assets/AustinsScripts/Asteroid.cs
@@ -12,7 +12,13 @@ public class Asteroid : MonoBehaviour
 	{
 
 		//transform.position= new Vector2(Random.Range(-10f,10f),Random.Range(-10f,10f));
-		transform.position=new Vector2(Mathf.PingPong(Random.Range(-10f,10),Random.Range(-10f,10f)),Mathf.PingPong(Random.Range(-10f,10f),Random.Range(-10f,10f)))* speed;
+		//transform.position = new Vector3(Mathf.PingPong(Time.time, 5), Random.Range(-10f, 10f), transform.position.z);
+		//transform.position=new Vector2(Mathf.PingPong(Random.Range(-10f,10),Random.Range(-10f,10f)),Mathf.PingPong(Random.Range(-10f,10f),Random.Range(-10f,10f)))* speed;
+	}
+
+	void Move()
+	{
+		//transform.position = new Vector2(Mathf.PingPong)(Time.time, 5), Random.Range(-10f, 10f, Random.Range(-10f,10f));
 	}
 
 
