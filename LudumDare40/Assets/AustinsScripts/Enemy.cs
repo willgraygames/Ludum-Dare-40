664ef78b5df4e5f557d5a025f58f9b04f24b9ba2
ArcticFire421 <ap682814@ohio.edu>
2017-12-01 23:59:55 -0500

    Enemy Move
    Enemy can move towards and look at the player

---
+++ LudumDare40/Assets/AustinsScripts/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour 
+{
+	public Transform target;
+	public float speed = 2f;
+	private float minDistance = 1f;
+	private float range;
+
+	void Update()
+	{
+
+		transform.LookAt (target);
+		range = Vector2.Distance (transform.position, target.position);
+		if (range > minDistance)
+		{
+			Debug.Log (range);
+			transform.position = Vector2.MoveTowards (transform.position, target.position, speed * Time.deltaTime);
+		}
+	}
+}
